3f35004908e8b0523020c83712e83ca773c37c86
Creativ <vietbao.telescope@gmail.com>
2023-04-30 15:17:23 +0700

    update

--- aws/terra/.template/main.tf
+++ aws/terra/.template/main.tf
@@ -1,21 +1,34 @@
-terraform {
-  backend "remote" {
-    organization = "${ORGANIZATION}"
-    workspaces {
-      name = "${WORKSPACE}"
-    }
-  }
-  required_version = "~> 0.14.3"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
+#terraform {
+#  backend "remote" {
+#    organization = "${ORGANIZATION}"
+#    workspaces {
+#      name = "${WORKSPACE}"
+#    }
+#  }
+#  required_version = "~> 0.14.3"
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 4.0"
+#    }
+#  }
+#}
+#
+#
+#provider "aws" {
+#  region = "us-east-1"
+#}
 
 
-provider "aws" {
-  region = "us-east-1"
+resource "null_resource" "nr" {
+  triggers = {
+    value = var.rid
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+        date >> /tmp/msg
+    EOT
+  }
 }
 
